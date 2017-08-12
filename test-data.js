7d537176bf5cb839196ae21414fd046356fa71d7
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-13 00:56:08 +0530

    Group test for helpful logs

--- test-data.js
+++ test-data.js
@@ -21,9 +21,11 @@ describe('metadata.txt', function () {
     return (fs.statSync(item).isDirectory() && whitelisted_directories.indexOf(item) === -1)
   })
 
-  modelDirs.forEach(function (modelDir) {
-    it('should be present for ' + modelDir, function () {
-      fs.existsSync(path.join(modelDir, 'metadata.txt')).should.be.true
+  describe('should be present', function () {
+    modelDirs.forEach(function (modelDir) {
+      it(modelDir, function () {
+        fs.existsSync(path.join(modelDir, 'metadata.txt')).should.be.true
+      })
     })
   })
 
@@ -33,18 +35,20 @@ describe('metadata.txt', function () {
     return path.join(modelDir, 'metadata.txt')
   })
 
-  metadataFiles.forEach(function (metaFile) {
-    it(metaFile + ' should be yaml readable', function (done) {
-      try {
-        yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
-        done()
-      } catch (e) {
-        done(e)
-      }
+  describe('should be yaml readable', function () {
+    metadataFiles.forEach(function (metaFile) {
+      it(metaFile, function (done) {
+        try {
+          yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
+          done()
+        } catch (e) {
+          done(e)
+        }
+      })
     })
   })
 
-  it('Team-model abbreviations should be unique', function (done) {
+  it('team-model abbreviations should be unique', function (done) {
     let abbreviations = metadataFiles.map(function (metaFile) {
       let meta = yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
       return meta.team_name + '-' + meta.model_abbr
@@ -67,17 +71,21 @@ describe('metadata.txt', function () {
     }
   })
 
-  metadataFiles.forEach(function (metaFile) {
-    it(metaFile + ' should have team_name within 10 chars', function () {
-      let meta = yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
-      meta.team_name.length.should.be.below(11)
+  describe('should have team_name withing 10 chars', function () {
+    metadataFiles.forEach(function (metaFile) {
+      it(metaFile, function () {
+        let meta = yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
+        meta.team_name.length.should.be.below(11)
+      })
     })
   })
 
-  metadataFiles.forEach(function (metaFile) {
-    it(metaFile + ' should have team_name within 15 chars', function () {
-      let meta = yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
-      meta.model_abbr.length.should.be.below(16)
+  describe('should have model_abbr within 15 chars', function () {
+    metadataFiles.forEach(function (metaFile) {
+      it(metaFile, function () {
+        let meta = yaml.safeLoad(fs.readFileSync(metaFile, 'utf8'))
+        meta.model_abbr.length.should.be.below(16)
+      })
     })
   })
 })
