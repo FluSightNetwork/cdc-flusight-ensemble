10e71cff90f3589489c046caf8bf6aaa1b8870e6
Abhinav Tushar <lepisma@fastmail.com>
2017-09-27 21:39:08 -0400

    Add getter for branding in panels.vue

--- flusight-deploy/2-build-flusight.sh
+++ flusight-deploy/2-build-flusight.sh
@@ -25,6 +25,7 @@ sed -i 's/span.brand.title-text {{ branding.title }}/a.brand.title-text(v-bind:h
 # CDC FluSight Network Collaborative Ensemble
 sed -i 's/| Real-time <b>Influenza Forecasts<\/b>/a(v-bind:href="branding.parentUrl") CDC FluSight Network/' ./src/components/Panels.vue
 sed -i 's/CDC FluSight Challenge/Collaborative Ensemble/' ./src/components/Panels.vue
+sed -i '/computed/a...mapGetters([\'branding\']),' ./src/components/Panels.vue
 
 # Change max heap size
 sed -i 's/node build\/build.js/node --max_old_space_size=6000 build\/build.js/' ./package.json
