7aea22bde38984ad54a2ccc03467efffbac915e7
Nicholas Reich <nick@schoolph.umass.edu>
2017-10-15 23:22:05 -0400

    minor changes for equal weights file

--- scripts/make-equal-weights.R
+++ scripts/make-equal-weights.R
@@ -6,11 +6,11 @@ model_names <- read.csv("../model-forecasts/component-models/model-id-map.csv",
     stringsAsFactors = FALSE)
 seasons <- paste0(2010:2016, "/", 2011:2017)
 
-weights <- expand.grid(
+equal_weights <- expand.grid(
     component_model_id = model_names$model.id,
     season = seasons,
     weight = 1/nrow(model_names)
     ) # Season * Model
 
-write.csv(weights, file="../scores/equal-weights.csv", 
+write.csv(equal_weights, file="../weights/equal-weights.csv", 
     quote = FALSE, row.names = FALSE)
