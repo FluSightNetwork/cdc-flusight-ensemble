7d12ac8e577f44b0d23575a5f81d70d999da174a
Abhinav Tushar <lepisma@fastmail.com>
2018-05-01 15:03:40 -0400

    Pull before pushing from travis

--- travis/viz.sh
+++ travis/viz.sh
@@ -17,4 +17,6 @@ git add .
 git commit -m "[TRAVIS] Auto deploy to GitHub pages from travis: ${SHA}"
 
 echo "> Pushing visualizer to gh-pages"
+# Pull if origin has new files
+git pull $SSH_REPO master --no-edit
 git push $SSH_REPO gh-pages --force
